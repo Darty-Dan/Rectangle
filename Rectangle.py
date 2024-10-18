8cf95d647b25993d8d3295bbe05757452a144f8b
Darty-Dan <2lindantaken@gmail.com>
2024-10-18 10:46:28 -0400

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,17 +1,12 @@
 #Rectangle function
-def rectangle(x, y, height, width, pointX, pointY):
-
-    #Check to make sure everything is a digit
-    if not (str(x).isdigit() and str(y).isdigit() and str(height).isdigit() and str(width).isdigit() and str(pointX).isdigit() and str(pointY).isdigit()):
-        print("Only use integer values")
-        coordinates = input("Enter point in \"(pointX,pointY)\" form: ")
-        rectangle(x, y, height, width, pointX, pointY)
+def rectangle(x, y, width, height, pointX, pointY):
 
     #Convert to integers for math
     x = int(x)
     y = int(y)
     height = int(height)
     width = int(width)
+    print(pointX)
     pointX = int(pointX)
     pointY = int(pointY)
 
@@ -29,15 +24,16 @@ def calculate_midpoints(x, y, Hrectangle, Wrectangle, pointX, pointY):
 
     #Midpoints calculations
     Fx = x
-    FyHyMy = (Hrectangle + y) / 2
+    FyHyMy = (Hrectangle + y)/2
     Hx = Wrectangle
-    IxGxMx = (Wrectangle + x) / 2
+    IxGxMx = (Wrectangle + x)/2
     Gy = y
     Iy = Hrectangle
 
     #Determine if the point is on or within the rectangle or on the edge or on a vertex
     if pointX >= x and pointX <= Wrectangle and pointY >= y and pointY <= Hrectangle:
         print("The point (%i, %i) is on or within the rectangle." % (pointX, pointY))
+
         #Check which quadrant
         if pointX > IxGxMx and pointX < Hx and pointY > FyHyMy and pointY < Iy:
             print("and it is within quadrant 1.")
@@ -47,6 +43,8 @@ def calculate_midpoints(x, y, Hrectangle, Wrectangle, pointX, pointY):
             print("and it is within quadrant 3.")
         elif pointX > IxGxMx and pointX < Hx and pointY > Gy and pointY < FyHyMy:
             print("and it is within quadrant 4.")
+
+        #Vertex check
         elif pointX == x and pointY == y:
             print("and it is on vertex A")
         elif pointX == x and pointY == Hrectangle:
@@ -55,6 +53,8 @@ def calculate_midpoints(x, y, Hrectangle, Wrectangle, pointX, pointY):
             print("and it is on vertex C")
         elif pointX == Wrectangle and pointY == y:
             print("and it is on vertex D")
+
+        #Line check
         elif pointX == x and pointY <= Hrectangle and pointY >= y:
             print("and it is on the line AB")
         elif pointX >= x and pointX <= Wrectangle and pointY == Hrectangle:
@@ -68,38 +68,54 @@ def calculate_midpoints(x, y, Hrectangle, Wrectangle, pointX, pointY):
     #Print midpoints
     print("The midpoints are F(%.2f, %.2f), I(%.2f, %.2f), H(%.2f, %.2f), G(%.2f, %.2f), M(%.2f, %.2f)" %(Fx, FyHyMy, IxGxMx, Iy, Hx, FyHyMy, IxGxMx, Gy, IxGxMx, FyHyMy))
 
-def Pinput():
+def pinput():
     #Input
-    coordinates = input("Enter point in \"(pointX,pointY)\" form: ")
+    coordinates = input("Enter point in \"(pointX,pointY)\" form(NO SPACES): ")
     bracket1 = coordinates.find("(")
     bracket2 = coordinates.find(")")
     comma = coordinates.find(",")
-    #Differentiate all the points needed
+
     if coordinates[0] != "(":
-        print("coordinates must start with (")
-        return Pinput()
+        print("Coordinates must start with (")
+        return pinput()
 
-    if coordinates[-1] != ")":
-        print("coordinates must end with )")
-        return Pinput()
+    elif coordinates[-1] != ")":
+        print("Coordinates must end with )")
+        return pinput()
 
-    if coordinates.count("(") > 1:
+    elif coordinates.count("(") > 1:
         print("There must only be one (")
+        return pinput()
 
-    if coordinates.count(")") > 1:
-        print("there must only be one )")
+    elif coordinates.count(")") > 1:
+        print("There must only be one )")
+        return pinput()
 
-    if comma == -1 or coordinates[bracket1 + 1] == "," or coordinates[bracket2 - 2] == ",":
+    elif comma == -1 or coordinates[bracket1 + 1] == "," or coordinates[bracket2 - 1] == ",":
         print("Comma must be between the 2 integers and there must be only 1 comma")
-        return Pinput()
+        return pinput()
 
-    if coordinates[comma + 1:bracket2] == "":
+    elif coordinates[comma + 1:bracket2] == "":
         print("Must have a second integer")
-        return Pinput()
+        return pinput()
 
-    pointX = coordinates[bracket1 + 1: comma]
+    elif coordinates.find(" ") != -1:
+        print("There cannot be a space in the coordinates")
+        return pinput()
+
+    elif coordinates.count(",") > 1:
+        print("There must only be one comma")
+
+    pointX = coordinates[bracket1 + 1:comma]
     pointY = coordinates[comma + 1:bracket2]
+
+    #Check for integer
+    if not (pointX.isdigit() and pointY.isdigit()):
+        print("Only use integer values")
+        return pinput()
+
     return pointX, pointY
 
-pointX, pointY = Pinput()
+#Assignment
+pointX, pointY = pinput()
 rectangle(3, 5, 17, 21, pointX, pointY)
